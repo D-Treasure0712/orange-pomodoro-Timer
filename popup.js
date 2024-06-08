4c895090b1216c5d8d8351196b31f38034c7aa8f
FujiKOut <kfj0121@gmail.com>
2024-06-08 22:47:27 +0900

    オレンジ，フォント，位置

--- popup.js
+++ popup.js
@@ -137,7 +137,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
   async function startAnimation() {
     startButton.style.display = 'none';
-    stopButton.style.display = '';
+    // stopButton.style.display = '';
     if (!reset_times) {
       reset_times = true;
       // smoothTransition(times);
