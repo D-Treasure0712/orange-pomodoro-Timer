6ee37e08c49a1490b8f142178368a412a731c076
optmew <154917988+optmew@users.noreply.github.com>
2024-06-08 14:53:27 +0900

    fixing variables

--- popup.js
+++ popup.js
@@ -1,8 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
-  var times = 25 * 60; // 残り秒数 (25分)
+  var times = 1 * 60; // 残り秒数 (25分)
+  var times_interval = 5 * 60 // interval time 5min
   changeTimes(times);
   var reset_times = false;
   var isTimerCompleted = false;
+  var interval_session = false;
 
   function changeTimes(seconds) {
     // var seconds = minutes * 60;
@@ -12,7 +14,7 @@ document.addEventListener('DOMContentLoaded', function() {
   function startAnimation() {
     if (!reset_times) {
       reset_times = true;
-      smoothTransition();
+      smoothTransition(times);
     }
   }
 
@@ -32,37 +34,37 @@ document.addEventListener('DOMContentLoaded', function() {
   }
 
 // 中心角を変更する関数
-function changeAngle(angle, times_animat) {
-  var circleElement = document.querySelector('.circle');
+  function changeAngle(angle, times_animat) {
+    var circleElement = document.querySelector('.circle');
 
-  if(angle < 65){
-    document.getElementById('triangle1').style.setProperty('--angle', angle);
-  }else if(angle < 125){
-    document.getElementById('triangle2').style.setProperty('--angle', angle - 60);
-  }else if(angle < 185){
-    document.getElementById('triangle3').style.setProperty('--angle', angle - 120);
-  }else if(angle < 245){
-    document.getElementById('triangle4').style.setProperty('--angle', angle - 180);
-  }else if(angle < 305){
-    document.getElementById('triangle5').style.setProperty('--angle', angle - 240);
-  }else if(angle < 360){
-    if(angle < 360){
-      document.getElementById('triangle6').style.setProperty('--angle', angle - 300);
+    if(angle < 65){
+      document.getElementById('triangle1').style.setProperty('--angle', angle);
+    }else if(angle < 125){
+      document.getElementById('triangle2').style.setProperty('--angle', angle - 60);
+    }else if(angle < 185){
+      document.getElementById('triangle3').style.setProperty('--angle', angle - 120);
+    }else if(angle < 245){
+      document.getElementById('triangle4').style.setProperty('--angle', angle - 180);
+    }else if(angle < 305){
+      document.getElementById('triangle5').style.setProperty('--angle', angle - 240);
+    }else if(angle < 360){
+      if(angle < 360){
+        document.getElementById('triangle6').style.setProperty('--angle', angle - 300);
+      }else{
+        document.getElementById('triangle6').style.setProperty('--angle', 65);
+      }
     }else{
-      document.getElementById('triangle6').style.setProperty('--angle', 65);
+      reset_angle();
     }
-  }else{
-    reset_angle();
-  }
 
-    // 現在の秒数を更新
-    let remainingSeconds = times_animat - (angle / 360 * times_animat);
-    if (remainingSeconds > 0) {
-      updateCurrentSeconds(remainingSeconds);
-    } else {
-      updateCurrentSeconds(0);
-    }
-}
+      // 現在の秒数を更新
+      let remainingSeconds = times_animat - (angle / 360 * times_animat);
+      if (remainingSeconds > 0) {
+        updateCurrentSeconds(remainingSeconds);
+      } else {
+        updateCurrentSeconds(0);
+      }
+  }
 
   function reset_angle() {
     var triangleElements = document.querySelectorAll('.triangle1, .triangle2, .triangle3, .triangle4, .triangle5, .triangle6');
@@ -75,9 +77,8 @@ function changeAngle(angle, times_animat) {
     reset_times = false;
   }
 
-  function smoothTransition() {
+  function smoothTransition(times_animat) {
     var currentAngle = 0;
-    var times_animat = document.getElementById('time').value * 60; // 分をカウント時間の秒数に変換
     var duration = 1000 * times_animat;
     var startTime = performance.now();
     var animation = setInterval(function() {
